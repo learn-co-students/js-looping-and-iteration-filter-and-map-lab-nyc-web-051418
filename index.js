89cff3a9987caaee951162900a4a08c0e27dbf2f
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-24 22:59:07 -0400

    Solved lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here:
+function driversWithRevenueOver(array, string) {
+
+  return array.filter( function (person) {
+    return person.revenue > string;
+
+  });
+
+}
+
+function driverNamesWithRevenueOver(array, string) {
+  const newArray = driversWithRevenueOver(array, string);
+
+  return newArray.map( function (person) {
+    return person.name;
+  });
+}
+
+function exactMatch(array, attribute) {
+
+  return array.filter( function (person) {
+    return person[Object.keys(attribute)[0]] === attribute[Object.keys(attribute)[0]];
+  });
+
+}
+
+function exactMatchToList(array, attribute) {
+  const newArray = exactMatch(array, attribute);
+
+  return newArray.map( function (person) {
+    return person.name;
+  });
+
+}
